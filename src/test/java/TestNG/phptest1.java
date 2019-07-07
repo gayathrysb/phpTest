c3b5b15f0cda6e26e53597d29f546cd3b7dd8522
gayathrysb <45505771+gayathrysb@users.noreply.github.com>
2019-07-07 21:34:34 +0530

    update test file

--- src/test/java/TestNG/phptest1.java
+++ src/test/java/TestNG/phptest1.java
@@ -6,15 +6,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import java.util.concurrent.TimeUnit;
 import org.testng.Assert;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class phptest1  {
-    WebDriver driver;
+    ChromeDriver driver;
 
     @BeforeMethod
     public void launch() {
         System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless");
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox");
+
+        driver = new ChromeDriver(options);
+
+
+
+        //driver.manage().window().maximize();
         driver.get("http://35.244.19.79:32768/");
         driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 
